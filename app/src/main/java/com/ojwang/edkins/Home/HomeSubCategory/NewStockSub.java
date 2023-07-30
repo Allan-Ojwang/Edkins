ee8b0decdb95c80b476fa1083a6aea5e46653307
Ojwang <68148967+Allan-Ojwang@users.noreply.github.com>
2023-07-30 03:31:04 +0300

    New Stock ui commit

---
+++ app/src/main/java/com/ojwang/edkins/Home/HomeSubCategory/NewStockSub.java
@@ -0,0 +1,14 @@
+package com.ojwang.edkins.Home.HomeSubCategory;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ojwang.edkins.R;
+
+public class NewStockSub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_stock_sub);
+    }
+}
